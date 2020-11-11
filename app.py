79cf9699331fa9c5f80bdb2850c7d764ea32e0fa
wasilema <mateusz.wasilewski@tomtom.com>
2020-11-12 00:02:30 +0100

    adjustment

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_session import Session
 import json
 app = Flask(__name__)
 app.secret_key = '786dsadhalhd7sady8asdhyaksdnas9'
-SESSION_TYPE = 'redis'
+SESSION_TYPE = ''
 app.config.from_object(__name__)
 Session(app)
 
