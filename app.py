56e8783f418823f096d064b73b17ac4fa3940210
wasilema <mateusz.wasilewski@tomtom.com>
2020-11-18 09:40:23 +0100

    E

--- app.py
+++ app.py
@@ -2,22 +2,38 @@ from flask import Flask, session, request
 from flask_session import Session
 import json
 from flask_cors import CORS, cross_origin
+import os
 app = Flask(__name__)
 app.secret_key = '786dsadhalhd7sady8asdhyaksdnas9'
 CORS(app) 
 SESSION_TYPE = 'filesystem'
 app.config.from_object(__name__)
 Session(app)
+
+MAIN_FILE_PATH = "DATA_.json"
+
 @cross_origin()
 @app.route('/saveUser', methods=['POST'])
 def saveUser():
     data = request.get_json()
-    if "users" in session:
-        print(session["users"])
-        session["users"].append(data)
+    if os.path.exists(MAIN_FILE_PATH):
+        data_so_far = dict()
+        with open(MAIN_FILE_PATH) as json_file:
+            data_so_far = json.load(json_file)
+        os.remove(MAIN_FILE_PATH)
+        print(data_so_far)
+        data_so_far["users"].append(data)
+        with open(MAIN_FILE_PATH,"w") as f:
+            f.write(json.dumps(data_so_far))
     else:
-        session["users"] = []
+        ew = dict()
+        with open(MAIN_FILE_PATH,"w") as f:
+            ew["users"] = []
+            ew["users"].append(data)
+            e_json = json.dumps(ew)
+            f.write(e_json)
     return ""
+
 @cross_origin()
 @app.route('/', methods=['GET'])
 def default():
@@ -26,17 +42,17 @@ def default():
 @cross_origin()
 @app.route('/flushSession', methods=['GET'])
 def flushSession():
-    if "users" in session:
-        session["users"] = []
-    else:
-        session["users"] = []
+    if os.path.exist(MAIN_FILE_PATH):
+         os.remove(MAIN_FILE_PATH)
     return ""
 
 @cross_origin()
 @app.route('/getUsersInfo', methods=['GET'])
 def getUserInfo():
-    if "users" in session:
-        return json.dumps(session["users"])
+    if os.path.exists(MAIN_FILE_PATH):
+        with open(MAIN_FILE_PATH) as json_file:
+            data = json.load(json_file)
+            return json.dumps(data["users"])
     else:
         return ""
 
